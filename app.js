9613f0a4d9a25255ed73ca29434b69552ad7761b
N0xzzt <austin2009irion@gmail.com>
2026-02-19 13:06:10 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,57 @@
+let plants = [
+    {
+     Name:"Zonnebloem",
+     Text:"zonnebloem Text",
+     Img: "nora'sFlora foto's/pexels-pixabay-54267.jpg",
+     Price:5
+    },
+{
+     Name:"Rode Tulpen",
+     Text:"Rode Tulpen Text",
+     Img:"nora'sFlora foto's/pexels-valeriya-1961778.jpg",
+     Price:7.68
+    },
+{
+     Name:"Cactus",
+     Text:"CactusText",
+     Img:"nora'sFlora foto's/pexels-scottwebb-403571.jpg",
+     Price:2
+    },
+{
+     Name:"Rozenboeket",
+     Text:"Rozenboeket Text",
+     Img:"nora'sFlora foto's/pexels-secret-garden-333350-931176.jpg",
+     Price:19.60
+    },
+{
+     Name:"Trouwdagboeket",
+     Text:"speciaal samengestelde trouwdag boeket bundel al vanaf €25",
+     Img:"nora'sFlora foto's/pexels-secret-garden-333350-931176.jpg",
+     Price:25
+    },
+{
+     Name:"Lenteboeket",
+     Text:"Lenteboeket al vanaf €20",
+     Img:"nora'sFlora foto's/pexels-secret-garden-333350-931176.jpg",
+     Price:20
+    },
+{   Name:"Diverse Planten",
+    Text:"Diverse Planten al vanaf €5 per pot",
+    Img:"nora'sFlora foto's/pexels-secret-garden-333350-931176.jpg",
+    Price:15
+    },
+{
+     Name:"Diverse lente bloemen",
+     Text:"Diverse lentebloemen €7,50 per bos",
+     Img:"nora'sFlora foto's/pexels-secret-garden-333350-931176.jpg",
+     Price:25
+    },
+{
+    Name:"Paarse allium Bloemen",
+    Text:"Paarse allium Bloemen per bos €4",
+    Img:"nora'sFlora foto's/pexels-secret-garden-333350-931176.jpg",
+    Price:10
+    },
+
+]
+;
